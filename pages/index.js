15bf80e5630c6874a7180fedeac059d1634e7d6d
Per Enström <enstrom.per@gmail.com>
2020-08-20 20:02:34 +0200

    Add content fetching from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { getHero } from '../lib/api';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Per Enström</title>
       </Head>
+      <pre>{JSON.stringify(props.hero, null, 2)}</pre>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const hero = await getHero();
+  return {
+    props: { hero }
+  };
 }
