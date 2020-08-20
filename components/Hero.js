e4ba0dfb4f2fc446badee1e22e83502d43343c0b
Per Enström <enstrom.per@gmail.com>
2020-08-20 22:04:15 +0200

    Add hero

---
+++ components/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 12.5rem;
+  background-image: ${(props) => `url("${props.image}")`};
+  background-size: cover;
+`;
+
+export const Hero = function Hero(props) {
+  const { heading, image } = props;
+
+  return (
+    <Wrapper image={image}>
+      <h1>{heading}</h1>
+    </Wrapper>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,24 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { getHero } from '../lib/api';
 
+// Shared components
+import { Hero } from '../components/Hero';
+
 export default function Home(props) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Per Enström</title>
       </Head>
+      <Hero
+        heading={props.hero.fields.heading}
+        image={props.hero.fields.image[0].secure_url}
+      />
+      <h2>Here is a h2</h2>
+      <h3>Here is a h3</h3>
+      <h4>Here is a h4</h4>
+      <h5>Here is a h5</h5>
+      <h6>Here is a h6</h6>
       <pre>{JSON.stringify(props.hero, null, 2)}</pre>
     </div>
   );
