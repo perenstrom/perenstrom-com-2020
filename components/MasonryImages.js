808d10203189a3daa9765c046b32f0b1306c70e5
Per Enström <enstrom.per@gmail.com>
2020-09-18 11:07:53 +0200

    Adjust gallery based on mobile

--- components/MasonryImages.js
+++ components/MasonryImages.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Masonry from 'react-masonry-component';
 import { LuminousGallery } from 'luminous-lightbox';
@@ -36,10 +36,38 @@ const masonryOptions = {
   transitionDuration: '0s'
 };
 
+const ImageComponent = (props) => {
+  const { isMobile, image } = props;
+
+  if (isMobile) {
+    return props.children;
+  } else {
+    return (
+      <a
+        className="lightbox-image"
+        href={getCloudinaryUrl(image.image, {
+          width: 1200,
+          imageType: IMAGE_TYPES.jpeg
+        })}
+      >
+        {props.children}
+      </a>
+    );
+  }
+};
+
 export const MasonryImages = React.memo(function MasonryImages(props) {
+  const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
-    if (document) {
-      new LuminousGallery(document.querySelectorAll('.lightbox-image'));
+    if (typeof window !== undefined && typeof document !== undefined) {
+      const mql = window?.matchMedia('(max-width: 480px)');
+      const isMobile = mql?.matches;
+      setIsMobile(isMobile);
+
+      if (!isMobile) {
+        new LuminousGallery(document.querySelectorAll('.lightbox-image'));
+      }
     }
   }, []);
 
@@ -51,18 +79,12 @@ export const MasonryImages = React.memo(function MasonryImages(props) {
             className="grid-item grid-sizer"
             key={image.image.public_id}
           >
-            <a
-              className="lightbox-image"
-              href={getCloudinaryUrl(image.image, {
-                width: 1200,
-                imageType: IMAGE_TYPES.jpeg
-              })}
-            >
+            <ImageComponent image={image} isMobile={isMobile}>
               <ResponsiveImage
                 cloudinaryImage={image.thumbnail}
                 width={Math.ceil((1280 - 3 * 16) / 3)}
               />
-            </a>
+            </ImageComponent>
           </ItemWrapper>
         ))}
       </Masonry>
